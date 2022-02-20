dcc57ef511783e8d79521f895cbe8adf94ee8ceb
Sergey Peretyagin <wodnyi33@yandex.ru>
2022-02-20 10:40:22 +0500

    commit 2

---
+++ src/main/java/Listener/Listener.java
@@ -0,0 +1,16 @@
+package Listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class Listener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+}
