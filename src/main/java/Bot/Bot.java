448e1544dcfe2be6a4d2d28e7fddaf66fa96993e
Sergey Peretyagin <wodnyi33@yandex.ru>
2022-02-20 10:42:18 +0500

    commit 3

---
+++ src/main/java/Bot/Bot.java
@@ -0,0 +1,24 @@
+package Bot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class Bot extends TelegramLongPollingBot {
+
+
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
