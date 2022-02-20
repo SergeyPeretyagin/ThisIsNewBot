85a2e7e4be4439c999778edd6a907f6ffb4951b0
Sergey Peretyagin <wodnyi33@yandex.ru>
2022-02-20 10:48:45 +0500

    commit 6

--- src/main/java/Listener/Listener.java
+++ src/main/java/Listener/Listener.java
@@ -1,12 +1,21 @@
 package Listener;
 
+import Bot.Bot;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
+@WebListener
 public class Listener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-
+        try {
+            Bot.start();
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
